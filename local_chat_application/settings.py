0fcd8beb11178a2dd6e3d1e781c84609c559f938
farzanebgr <farzenehboogari@gmail.com>
2022-09-13 16:04:31 +0430

    create user model

--- local_chat_application/settings.py
+++ local_chat_application/settings.py
@@ -114,3 +114,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# custom User model
+AUTH_USER_MODEL = 'user_app.User'
+
--- user_app/models.py
+++ user_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    user_avatar = models.ImageField(blank=True,null=True)
+    user_phone = models.CharField(max_length=20)
+    user_gender = models.BooleanField(blank=True,null=True)
+    user_status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
